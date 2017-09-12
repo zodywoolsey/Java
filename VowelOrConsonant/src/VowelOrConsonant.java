769b49b56286653180d730b0360dd2dadfbe0870
zodywoolsey <zodywoolsey@gmail.com>
2017-09-12 12:37:35 -0500

    misc

---
+++ VowelOrConsonant/src/VowelOrConsonant.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author zodywoolsey
+ */
+import java.util.Scanner;
+public class VowelOrConsonant {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char lett;
+        
+        
+        
+    }
+    
+}
